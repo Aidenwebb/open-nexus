6422a2684e63c83b280b43991361ec50c40d3d2e
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-09-15 21:35:22 +0100

    Add ResponseModels

---
+++ OpenNexus/src/Application/Companies/Models/Response/ListResponseModel.cs
@@ -0,0 +1,15 @@
+using Arnkels.OpenNexus.Domain.Models.Api;
+
+namespace Arnkels.OpenNexus.Application.Companies.Models.Response;
+
+public class ListResponseModel<T> : ResponseModel where T : ResponseModel
+{
+    public ListResponseModel(IEnumerable<T> data, string continuationToken = null) : base("list")
+    {
+        Data = data;
+        ContinuationToken = continuationToken;
+    }
+
+    public IEnumerable<T> Data { get; set; }
+    public string ContinuationToken { get; set; }
+}
