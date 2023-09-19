833aae957bfbc2243685cb6595d6c975d9853439
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-09-19 18:09:22 +0100

    Stop using AutoMapper

--- OpenNexus/src/Application/Companies/Models/Response/CompanyResponseModel.cs
+++ OpenNexus/src/Application/Companies/Models/Response/CompanyResponseModel.cs
@@ -6,9 +6,45 @@ namespace Arnkels.OpenNexus.Application.Companies.Models.Response;
 
 public class CompanyResponseModel : ResponseModel
 {
-    public CompanyResponseModel() : base("company")
+    public CompanyResponseModel(Company company) : base("company")
     {
+        if (company == null)
+        {
+            throw new ArgumentNullException(nameof(company));
+        }
+
+        Id = company.Id;
+        Identifier = company.Identifier;
+        Name = company.Name;
+        Status = new CompanyStatusResponseModel(company.Status);
+        Types = new List<CompanyType>(company.Types);
+        AddressLine1 = company.AddressLine1;
+        AddressLine2 = company.AddressLine2;
+        AddressLine3 = company.AddressLine3;
+        City = company.City;
+        State = company.State;
+        Country = company.Country;
+        Zip = company.Zip;
+        PhoneNumber = company.PhoneNumber;
+        FaxNumber = company.FaxNumber;
+        MobileNumber = company.MobileNumber;
+        WebsiteUri = company.WebsiteUri;
+        AccountNumber = company.AccountNumber;
+        ParentCompany = company.ParentCompanyId == null ? null : new CompanyResponseModelMinimal(company.ParentCompany);
+        AnnualRevenue = company.AnnualRevenue;
+        NumberOfEmployees = company.NumberOfEmployees;
+        YearEstablished = company.YearEstablished;
     }
+
+    /// <summary>
+    /// Internal Company Id
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// A Human Readable Identifier
+    /// </summary>
+    public string Identifier { get; set; }
 
     /// <summary>
     /// The Company's Name
@@ -16,40 +52,60 @@ public class CompanyResponseModel : ResponseModel
     public string Name { get; set; }
 
     /// <summary>
-    /// Navigation Property for the companies types
-    /// </summary>
-    public virtual List<CompanyType>? Types { get; } = new();
-
-    /// <summary>
-    /// Unique identifier for the companies status. Maps to the CompanyStatus Entity
-    /// </summary>
-    public Guid CompanyStatusId { get; set; }
-
-    /// <summary>
     /// Navigation Property for the companies status
     /// </summary>
-    public virtual CompanyStatus Status { get; set; }
+    public virtual CompanyStatusResponseModel Status { get; set; }
+
+    /// <summary>
+    /// Navigation Property for the companies types
+    /// </summary>
+    public virtual List<CompanyType>? Types { get; set; }
 
     public string? AddressLine1 { get; set; }
     public string? AddressLine2 { get; set; }
     public string? AddressLine3 { get; set; }
     public string? City { get; set; }
+    public string? State { get; set; }
     public string? Country { get; set; }
-    public string? PostCode { get; set; }
+    public string? Zip { get; set; }
     public string? PhoneNumber { get; set; }
     public string? FaxNumber { get; set; }
     public string? MobileNumber { get; set; }
     public string? WebsiteUri { get; set; }
     public string? AccountNumber { get; set; }
-    public int? NumberOfEmployees { get; set; }
+    public CompanyResponseModelMinimal? ParentCompany { get; set; }
     public int? AnnualRevenue { get; set; }
+    public int? NumberOfEmployees { get; set; }
     public int? YearEstablished { get; set; }
 }
 
-public class CompanyMapperProfile : Profile
+public class CompanyResponseModelMinimal : ResponseModel
 {
-    public CompanyMapperProfile()
+    public CompanyResponseModelMinimal(Company company) : base("company")
     {
-        CreateMap<Company, CompanyResponseModel>().ReverseMap();
+        Id = company.Id;
+        Name = company.Name;
+        Identifier = company.Identifier;
+        Status = new CompanyStatusResponseModel(company.Status);
+        ParentCompany = company.ParentCompanyId == null ? null : new CompanyResponseModelMinimal(company.ParentCompany);
     }
+
+    /// <summary>
+    /// Internal Company Id
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// The Company's Name
+    /// </summary>
+    public string Name { get; set; }
+
+    public string Identifier { get; set; }
+
+    /// <summary>
+    /// Navigation Property for the companies status
+    /// </summary>
+    public virtual CompanyStatusResponseModel Status { get; set; }
+
+    public CompanyResponseModelMinimal? ParentCompany { get; set; }
 }
