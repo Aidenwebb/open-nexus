ec6dfbe9c9d642e569f5d876756ab7edd9ec5cf4
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-09-21 12:24:51 +0100

    Add FluentValidator and MediatR to AppServices

--- OpenNexus/src/Application/ConfigureServices.cs
+++ OpenNexus/src/Application/ConfigureServices.cs
@@ -2,6 +2,9 @@ using System.Reflection;
 using Arnkels.OpenNexus.Application.Services;
 using Arnkels.OpenNexus.Domain.Services;
 using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using MediatR;
+
 
 namespace Arnkels.OpenNexus.Application;
 
@@ -10,6 +13,8 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()); });
         services.AddScoped<ICompanyService, CompanyService>();
         services.AddScoped<ICompanyStatusService, CompanyStatusService>();
 
